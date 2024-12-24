fdf6c27ab64630f4306e451310b8c3167317d24b
devlikebear <devlikebear@gmail.com>
2024-12-24 17:34:02 +0900

    데이터베이스 마이그레이션 추가 및 포스트 컬렉션 생성

--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"embed"
 	"log"
 
+	_ "mini-blog-backend/migrations"
+
 	"github.com/labstack/echo/v5"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/plugins/migratecmd"
 )
 
 //go:embed all:frontend/build
@@ -16,6 +19,10 @@ var distDirFS = echo.MustSubFS(frontend, "frontend/build")
 
 func main() {
 	app := pocketbase.New()
+
+	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
+		Automigrate: true,
+	})
 
 	// Integrate Echo router with PocketBase
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
---
+++ migrations/1735029148_created_posts.go
@@ -0,0 +1,74 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
+	m "github.com/pocketbase/pocketbase/migrations"
+	"github.com/pocketbase/pocketbase/models"
+)
+
+func init() {
+	m.Register(func(db dbx.Builder) error {
+		jsonData := `{
+			"id": "t71gq3b6i3mbmbj",
+			"created": "2024-12-24 08:32:28.781Z",
+			"updated": "2024-12-24 08:32:28.781Z",
+			"name": "posts",
+			"type": "base",
+			"system": false,
+			"schema": [
+				{
+					"system": false,
+					"id": "yenfacrn",
+					"name": "title",
+					"type": "text",
+					"required": false,
+					"presentable": false,
+					"unique": false,
+					"options": {
+						"min": null,
+						"max": null,
+						"pattern": ""
+					}
+				},
+				{
+					"system": false,
+					"id": "hgmyoog2",
+					"name": "contents",
+					"type": "editor",
+					"required": false,
+					"presentable": false,
+					"unique": false,
+					"options": {
+						"convertUrls": false
+					}
+				}
+			],
+			"indexes": [],
+			"listRule": null,
+			"viewRule": null,
+			"createRule": null,
+			"updateRule": null,
+			"deleteRule": null,
+			"options": {}
+		}`
+
+		collection := &models.Collection{}
+		if err := json.Unmarshal([]byte(jsonData), &collection); err != nil {
+			return err
+		}
+
+		return daos.New(db).SaveCollection(collection)
+	}, func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("t71gq3b6i3mbmbj")
+		if err != nil {
+			return err
+		}
+
+		return dao.DeleteCollection(collection)
+	})
+}
